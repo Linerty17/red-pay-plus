558fb376d5ba655308f3152f28dbdd9acd29c1d9
Linerty17 <187655124+Linerty17@users.noreply.github.com>
2025-11-22 20:19:54 +0100

    Update payment account details in PaymentInstructions

--- src/pages/PaymentInstructions.tsx
+++ src/pages/PaymentInstructions.tsx
@@ -19,9 +19,9 @@ const PaymentInstructions = () => {
   const [showFailure, setShowFailure] = useState(false);
 
   const amount = "6,700";
-  const accountNumber = "1100722244";
-  const bankName = "Kolomoni mfb";
-  const accountName = "Okumefuna ikechukwu celestine";
+  const accountNumber = "5569742889";
+  const bankName = "Moniepoint mfb";
+  const accountName = "chinemerem liberty sunday";
   const referenceId = `REF${Date.now()}`;
 
   const copyToClipboard = (text: string, field: string) => {
