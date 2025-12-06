f038557fed09f513b1518f5881fd779dc7e5fe3d
Linerty17 <187655124+Linerty17@users.noreply.github.com>
2025-12-06 18:26:49 +0100

    Update WhatsApp support link

--- src/pages/Support.tsx
+++ src/pages/Support.tsx
@@ -14,7 +14,7 @@ const Support = () => {
   };
 
   const handleWhatsAppSupport = () => {
-    window.open("https://wa.me/2349029987533", "_blank");
+    window.open("https://wa.me/2348109375382", "_blank");
   };
 
   const handleEmailSupport = () => {
